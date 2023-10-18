b90a1c546b29c703b28774c062f0d1c996938c48
giuseppeandreacosta <giuseppeandreac@gmail.com>
2023-10-18 20:01:13 +0200

    risposte su react

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+
+// 1.Cos’è ReactJS? (risposta aperta)
+
+
+// React è un framework di sviluppo realizzato da Facebook,è OS(open source).
+// Serve per creare app veloci e facilmente leggibili.
+// Non manipola direttamente il DOM come javascript.
+
+
+// 2. ReactJS è un framework, non una libreria. (v/f) 
+// Falso.
+// ReactJS fornisce molte funzionalità che si potrebbero trovare in un framework,ma viene classificato come libreria perchè gli sviluppatori hanno il controllo del flusso dell'applicazione.
+
+
+// 3. Il file package.json contiene…
+
+// Molte informazioni utili, come ad esempio l’elenco di tutte le dipendenze richieste dall’applicazione 
+
+
+// 4. create-react-app è l’unico modo possibile per creare un’applicazione React (v/f)
+
+//  Falso.
+
+// 5. Qual è il comando da lanciare nel terminale per creare una nuova create-react-app con nome “test”?
+// npx create-react-app test 
+
+
+// 6. Cos’è un componente React?
+
+// Un blocco di logica/contenuto riutilizzabile all’interno dell’applicazione
+
+
+// 7. Un componente React può venire creato in tre modi: come funzione, classe o interfaccia. (v/f)
+
+// Falso, un conpotente può essere creato come una funzione o classe.
+
+// 8. Qual è la differenza tra componenti creati come funzione e componenti creati come classe?
+// I componenti creati come funzione sono più semplici da scrivere ma con meno funzionalità, 
+// mentre i componenti creati come classe necessitano in genere di un maggior numero di linee di codice ma possiedono più funzionalità.
+
+// 9. Le props sono frammenti di informazione assegnati all’invocazione di un componente React, utili al fine di rendere il componente dinamico e più riutilizzabile. (v/f)
+
+// Vero  Le props in React sono frammenti di informazioni che vengono passati ai componenti. 
+// Questi dati permettono di rendere i componenti più dinamici e riutilizzabili. Le props possono includere qualsiasi tipo di dati validi in JavaScript.
+
+// 10. Le props possono essere passate solamente da un componente genitore ad un componente figlio, non è possibile fare il contrario (v/f)
+
+// Vero. In React, le props vengono passate da un componente genitore a un componente figlio. Non è possibile fare il contrario,
+// ovvero passare le props da un componente figlio a un componente genitore. 
+// Se c’è la necessità di comunicare dati dal figlio al genitore, si può utilizzare una funzione passata come prop.
+
+// 11. Da dove possono venire recuperate le props all’interno di un componente React creato come classe?
+
+// Possono essere recuperate all’interno dell' oggetto ‘this’, dentro un sotto-oggetto chiamato ‘props’
